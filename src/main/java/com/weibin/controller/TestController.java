44a32d90c98dc6d1627e9d0c0fc324271e801877
zengbin <596659597@qq.com>
2025-03-07 19:32:56 +0800

    增加SpringMVC依赖

--- src/main/java/com/weibin/controller/TestController.java
+++ src/main/java/com/weibin/controller/TestController.java
@@ -1,16 +1,23 @@
 package com.weibin.controller;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.time.LocalDateTime;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-public class TestController extends HttpServlet {
-    
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/plain;charset=UTF-8");
-        resp.getWriter().write("Hello! Current time is: " + LocalDateTime.now());
+@Controller
+@RequestMapping("/api")
+public class TestController {
+    // @RequestMapping作用在方法上，不再区分get/post请求
+    @RequestMapping("/test")
+    @ResponseBody
+    public String getTest() {
+        return "Get Method Test! Current time is: ";
+    }
+
+    @PostMapping("/post_test")
+    @ResponseBody
+    public String postTest() {
+        return "Post Method Test! Current time is: ";
     }
 }
