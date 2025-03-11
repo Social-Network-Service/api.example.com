e1d548166873149cc4d9d294efab06ca25adc44e
zengbin <596659597@qq.com>
2025-03-11 19:18:41 +0800

    update

---
+++ src/sql/video.sql
@@ -0,0 +1,3 @@
+insert into video (video_name,video_url) values ('视频1','url1');
+insert into video (video_name,video_url) values ('视频2','url2');
+insert into video (video_name,video_url) values ('视频3','url3');
